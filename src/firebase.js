834a348815c3b0703b84811ac1638c50160234fd
codesage <laughingsagecoder@gmail.com>
2024-01-09 23:37:51 +0200

    optimizing firebase import for prod

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,18 @@
-import firebase from 'firebase';
+// import firebase from 'firebase';
 
-const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
-};
+// const firebaseConfig = {
+//   apiKey: "",
+//   authDomain: "",
+//   projectId: "",
+//   storageBucket: "",
+//   messagingSenderId: "",
+//   appId: "",
+//   measurementId: ""
+// };
 
-firebase.initializeApp(firebaseConfig);
-const storage = firebase.storage();
+// firebase.initializeApp(firebaseConfig);
+// const storage = firebase.storage();
+// export default storage;
+
+import storage from 'firebase/firebase-storage';
 export default storage;
